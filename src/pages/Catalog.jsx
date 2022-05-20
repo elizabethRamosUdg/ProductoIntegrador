42acf21db88a4c7d87e40c2f0b90b56ae6422534
elizabethRamosUdg <elizabeth.ramos@udgvirtual.udg.mx>
2022-05-19 22:02:09 -0500

    Creada la estructura del proyect, menu y paginas

---
+++ src/pages/Catalog.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Product from '../components/Product/Product';
+
+class Catalog extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container main-content">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    );
+  }
+}
+
+
+export default Catalog;
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-3">Nosotros </h1>
+            <p>
+                Somos una comunidad en donde fomentamos el conocimiento colectivo.
+                <br/>
+                Solo de esta manera se formarán profesionales que cambien sus vidas y tengan un mejor futuro.
+            </p>
+            <p><a className="btn btn-primary btn-lg" href="https://nubecolectiva.com" target="_blank" role="button">Visitar Web</a></p>
+        </div>
+    </div>
+    );
+}
+
+export default Home;
+
